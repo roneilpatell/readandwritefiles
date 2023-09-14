3f6c1c14fbdebfd40c267ab9f853f13649d9d577
Roneil Patel <roneil_patel1@baylor.edu>
2023-09-14 17:47:27 -0500

    914

---
+++ read_philosphers2.py
@@ -0,0 +1,16 @@
+def main():
+    infile = open('philosphers.txt', 'r')
+
+    line1 = infile.readline().rstrip('\n')
+    line2 = infile.readline().rstrip('\n')
+    line3 = infile.readline().rstrip('\n')
+
+
+    print(line1)
+    print(line2)
+    print(line3)
+
+
+
+main()
+
